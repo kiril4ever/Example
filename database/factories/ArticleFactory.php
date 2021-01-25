9d69ae3ad0a94a0a26a360208aafa9ce98e0226c
kiril4ever <chirila.daniels@gmail.com>
2021-01-25 13:18:35 +0200

    Add files via upload

---
+++ database/factories/ArticleFactory.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Article;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ArticleFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Article::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->text(30),
+            'type' => $this->faker->text(10),
+            'color' => $this->faker->text(10),
+            'price' => $this->faker->randomFloat(10)
+        ];
+    }
+}
